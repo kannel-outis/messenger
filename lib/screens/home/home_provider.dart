afcd084c7b93720ca6c45bc6644f8cd55074376f
khaniel-outis <emirbadoo@gmail.com>
2021-02-05 14:46:56 -0800

    home_provider

---
+++ lib/screens/home/home_provider.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:messenger/models/chat.dart';
+import 'package:messenger/models/user.dart';
+import 'package:messenger/services/offline/hive.db/hive_handler.dart';
+import 'package:messenger/services/offline/hive.db/models/hive_chat.dart';
+import 'package:messenger/services/offline/shared_prefs/shared_prefs.dart';
+import 'package:messenger/services/online/firebase/firestore_service.dart';
+import '../../services/online/mqtt/mqtt_handler.dart';
+import 'package:messenger/services/online/online.dart';
+import 'package:messenger/utils/constants.dart';
+
+class HomeProvider extends ChangeNotifier {
+  Online _storeService = FireStoreService();
+  MQTThandler _mqttHandler = MQTThandler();
+  HiveHandler _hiveHandler = HiveHandler();
+  bool isme(String userID) {
+    final User prefUser = User.fromMap(
+        json.decode(SharedPrefs.instance.getString(OfflineConstants.MY_DATA)));
+    return prefUser.id == userID;
+  }
+
+  void listenTocloudStreamAndSubscribeTopic() {
+    _storeService
+        .listenWhenAUserInitializesAChat(_mqttHandler.user)
+        .listen((event) {
+      event.docChanges.forEach((element) {
+        print(element.doc.data());
+      });
+      if (event.docChanges.isNotEmpty) {
+        event.docChanges.forEach((element) {
+          Chat chat = Chat.froMap(element.doc.data());
+          bool exists = _hiveHandler.checkIfchatExists(HiveChat(
+            chatId: chat.chatID,
+            participants: List<User>.from(chat.participants),
+          ));
+          if (exists == false) {
+            _hiveHandler.saveChatToDB(chat);
+          }
+          _mqttHandler
+              .subscribe(chat.chatID)
+              .then((value) => print('SubScribed'));
+        });
+      } else {
+        print('Empty');
+      }
+    });
+  }
+}
