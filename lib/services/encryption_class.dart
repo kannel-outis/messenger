864a5e0f6e9b641344704784cd11d6acfd539984
khaniel-outis <enikuomehinoriade@gmail.com>
2021-04-26 03:25:58 -0700

    refactored encrypt class #21

--- lib/services/encryption_class.dart
+++ lib/services/encryption_class.dart
@@ -19,22 +19,19 @@ class EncryptClassHandler extends ManagerHandler<EncryptClass?> {
   EncryptClassHandler() {
     setManager(EncryptClass.instance);
   }
-  @override
-  EncryptClass? setManager(EncryptClass? newManager) {
+
+  Manager? setManager(Manager? newManager) {
     return super.setManager(newManager);
   }
 
-  @override
   Uint8List rsaEncrypt(RSAPublicKey myPublic, Uint8List dataToEncrypt) {
     return manager!.rsaEncrypt(myPublic, dataToEncrypt);
   }
 
-  @override
   Uint8List rsaDecrypt(RSAPrivateKey myPrivate, Uint8List cipherText) {
     return manager!.rsaDecrypt(myPrivate, cipherText);
   }
 
-  @override
   AsymmetricKeyPair<MyPublicKey, MyPrivateKey> generateKeyPairs(
       {SecureRandom? secureRandom, int bitLength = 2048}) {
     return manager!.generateKeyPairs(secureRandom: _exampleSecureRandom());
@@ -53,7 +50,6 @@ class EncryptClassHandler extends ManagerHandler<EncryptClass?> {
     return secureRandom;
   }
 
-  @override
   String? keyToString({RSAAsymmetricKey? key}) {
     return manager!.keyToString(key: key);
   }
